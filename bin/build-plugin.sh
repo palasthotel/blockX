27ed1b09061ef3cf575e2abe6f86f3468435ca00
EdwardBock <edward.bock@palasthotel.de>
2020-10-29 19:55:32 +0100

    plugin builder

---
+++ bin/build-plugin.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+PLUGIN_SLUG="blockx"
+PROJECT_PATH=$(pwd)
+BUILD_PATH="${PROJECT_PATH}/build"
+DEST_PATH="$BUILD_PATH/$PLUGIN_SLUG"
+
+echo "Generating build directory..."
+rm -rf "$BUILD_PATH"
+mkdir -p "$DEST_PATH"
+
+echo "Syncing files..."
+rsync -rL "$PROJECT_PATH/public/" "$DEST_PATH/"
+
+echo "Cleanup files..."
+rm "$DEST_PATH/composer.json"
+
+echo "Generating zip file..."
+cd "$BUILD_PATH" || exit
+zip -q -r "${PLUGIN_SLUG}.zip" "$PLUGIN_SLUG/"
+
+cd "$PROJECT_PATH" || exit
+mv "$BUILD_PATH/${PLUGIN_SLUG}.zip" "$PROJECT_PATH"
+echo "${PLUGIN_SLUG}.zip file generated!"
+
+echo "Cleanup build path..."
+rm -rf "$BUILD_PATH"
+
+echo "Build done!"
