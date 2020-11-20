48bb5c0ed5117de0a91a4b11a6cecee526c18ae0
EdwardBock <edward.bock@palasthotel.de>
2020-11-20 14:07:50 +0100

    transpiled

--- public/js/gutenberg/blockx.asset.php
+++ public/js/gutenberg/blockx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill', 'wp-server-side-render'), 'version' => '771e75b788c165135617888106867e4e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'f92593d36c4c2bbc93b3b09f4ce2856d');
