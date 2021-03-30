5f0ea9b39e21c68e1f9790b9cb534c30fe3c6cdb
EdwardBock <edward.bock@palasthotel.de>
2021-03-30 11:56:43 +0200

    auto format

--- public/classes/Widgets/Text.php
+++ public/classes/Widgets/Text.php
@@ -12,29 +12,33 @@ class Text extends _Widget {
 	private $help = "";
 
 	public static function build( string $key, string $label, string $defaultValue = "" ) {
-		return new static($key, $label, static::TYPE, $defaultValue);
+		return new static( $key, $label, static::TYPE, $defaultValue );
 	}
 
-	public function rows(int $rows){
-		try{
-			if($rows <= 0) throw new \Exception("BlockX Text Widget rows needs to be larger than 0 but is $rows");
+	public function rows( int $rows ) {
+		try {
+			if ( $rows <= 0 ) {
+				throw new \Exception( "BlockX Text Widget rows needs to be larger than 0 but is $rows" );
+			}
 			$this->rows = $rows;
-		} catch (\Exception $exception){
-			error_log($exception->getMessage());
+		} catch ( \Exception $exception ) {
+			error_log( $exception->getMessage() );
 		}
 
 		return $this;
 	}
 
-	public function help(string $help){
+	public function help( string $help ) {
 		$this->help = $help;
+
 		return $this;
 	}
 
 	public function toArray() {
-		$arr = parent::toArray();
+		$arr         = parent::toArray();
 		$arr["rows"] = $this->rows;
 		$arr["help"] = $this->help;
+
 		return $arr;
 	}
 
