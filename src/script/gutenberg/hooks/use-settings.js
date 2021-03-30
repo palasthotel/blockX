43ce56601b8035c509311fb1427388926ded80d1
EdwardBock <edward.bock@palasthotel.de>
2021-03-30 12:58:44 +0200

    use auto save timeout

--- src/script/gutenberg/hooks/use-settings.js
+++ src/script/gutenberg/hooks/use-settings.js
@@ -1,8 +1,14 @@
+import {useEffect} from "@wordpress/element";
 
-export const useSettings = () => {
-    return BlockX.settings;
-}
-
-export const useAutoSaveTimeout = ()=>{
-    return useSettings().auto_save_timeout;
+export const useAutoSaveTimeout = (fn, changes) => {
+    const autoSaveTimeout = BlockX.settings.auto_save_timeout;
+    useEffect(() => {
+        let timeoutId = null;
+        if (Object.keys(changes).length !== 0 && autoSaveTimeout > 100) {
+            timeoutId = setTimeout(() => {
+                fn();
+            }, autoSaveTimeout);
+        }
+        return () => clearTimeout(timeoutId);
+    }, [JSON.stringify(changes)]);
 }
