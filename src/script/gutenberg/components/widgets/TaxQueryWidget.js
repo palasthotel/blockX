a52946b2aa6ca82c064d2f7eff902d9127f6f7c1
EdwardBock <edward.bock@palasthotel.de>
2021-05-05 08:52:54 +0200

    refactoring

--- src/script/gutenberg/components/widgets/TaxQueryWidget.js
+++ src/script/gutenberg/components/widgets/TaxQueryWidget.js
@@ -1,11 +1,11 @@
-import { Button, SelectControl, ToggleControl, FormTokenField, BaseControl } from "@wordpress/components";
+import {Button, SelectControl, ToggleControl, FormTokenField, BaseControl} from "@wordpress/components";
 import {useEffect, useState} from "@wordpress/element";
 import {useFetchTaxonomyTerms, useFetchTaxonomyTermsByIds} from "../../hooks/use-taxonomy.js";
-import { useTranslationWidgetTaxQuery } from '../../hooks/use-translation.js'
-import { buildOption } from "../../utils/select.js";
+import {useTranslationWidgetTaxQuery} from '../../hooks/use-translation.js'
+import {buildOption} from "../../utils/select.js";
 import './TaxQueryWidget.css';
 
-const findTermByName = (name, terms) => terms.find(_t=>_t.name === name);
+const findTermByName = (name, terms) => terms.find(_t => _t.name === name);
 const findTermBySlug = (slug, terms) => terms.find(_t => _t.slug === slug);
 const findTermById = (id, terms) => terms.find(_t => _t.id === id);
 const findTerm = (s, terms) => findTermById(s, terms) || findTermBySlug(s, terms) || findTermByName(s, terms);
@@ -32,8 +32,8 @@ const ConditionControl = ({taxonomies, value, onChange}) => {
     // ------------------------------------------------
     // auto select default taxonomy effect
     // ------------------------------------------------
-    useEffect(()=>{
-        if(!taxonomies.map(tax=>tax.value).includes(taxonomy)){
+    useEffect(() => {
+        if (!taxonomies.map(tax => tax.value).includes(taxonomy)) {
             onChange({
                 ...value,
                 taxonomy: taxonomies[0].value,
@@ -44,35 +44,35 @@ const ConditionControl = ({taxonomies, value, onChange}) => {
     // ------------------------------------------------
     // suggestions
     // ------------------------------------------------
-    useEffect(()=>{
-        let requestTimeout = setTimeout(()=>{
+    useEffect(() => {
+        let requestTimeout = setTimeout(() => {
             setSearchTermsInput(tokenInput);
         }, 600);
-       return ()=> clearTimeout(requestTimeout);
+        return () => clearTimeout(requestTimeout);
     }, [termIds, tokenInput]);
 
     // ------------------------------------------------
     // taxonomy token field changed
     // ------------------------------------------------
-    const handleChangeTerms = (_terms)=>{
-        const newTerms = _terms.map(t=>{
+    const handleChangeTerms = (_terms) => {
+        const newTerms = _terms.map(t => {
             const search = typeof t === typeof "" ? t : t.value;
             const _term = findTerm(search, taxonomyTerms) || findTerm(search, selectedTerms);
             return _term ? _term.id : search;
         });
         onChange({
             ...value,
-            termIds:newTerms
+            termIds: newTerms
         })
     }
 
-    const handleTokenInputChange = (value)=>{
+    const handleTokenInputChange = (value) => {
         setTokenInput(value);
     }
 
     const cls = ["blockx--tax-query"];
-    if(isResolvingTaxonomyTerms) cls.push("is-resolving-search");
-    if(isResolvingSelectedTerms) cls.push("is-resolving-selection");
+    if (isResolvingTaxonomyTerms) cls.push("is-resolving-search");
+    if (isResolvingSelectedTerms) cls.push("is-resolving-selection");
 
     return <div className={cls.join(" ")}>
         <div>
@@ -80,7 +80,7 @@ const ConditionControl = ({taxonomies, value, onChange}) => {
                 label={label_taxonomy}
                 options={taxonomies}
                 value={taxonomy}
-                onChange={(taxonomy)=>onChange({
+                onChange={(taxonomy) => onChange({
                     ...value,
                     taxonomy
                 })}
@@ -89,21 +89,21 @@ const ConditionControl = ({taxonomies, value, onChange}) => {
         <div>
             <FormTokenField
                 label={label_add_terms}
-                value={termIds.map(t=>{
+                value={termIds.map(t => {
                     const taxonomyTerm = findTerm(t, selectedTerms)
                     return taxonomyTerm ? taxonomyTerm.name : t;
                 })}
                 onInputChange={handleTokenInputChange}
-                suggestions={taxonomyTerms.map(t=>(t.name))}
+                suggestions={taxonomyTerms.map(t => (t.name))}
                 onChange={handleChangeTerms}
             />
         </div>
         <div>
-            <SelectControl 
+            <SelectControl
                 label={label_operator}
                 value={operator}
-                options={['IN', 'NOT IN', 'AND' ].map(i=> buildOption(i,i))}
-                onChange={(_operator)=>{
+                options={['IN', 'NOT IN', 'AND'].map(i => buildOption(i, i))}
+                onChange={(_operator) => {
                     onChange({
                         ...value,
                         operator: _operator,
@@ -114,7 +114,7 @@ const ConditionControl = ({taxonomies, value, onChange}) => {
     </div>
 }
 
-const ConditionWrapper = ({children})=>{
+const ConditionWrapper = ({children}) => {
     return <div
         style={{
             background: "rgba(0, 0, 0, 0.02)",
@@ -130,7 +130,7 @@ const ConditionWrapper = ({children})=>{
     </div>
 }
 
-const ConditionGroup = ({taxonomies, value, onChange})=>{
+const ConditionGroup = ({taxonomies, value, onChange}) => {
 
     const {
         toggle_AND_description,
@@ -140,32 +140,32 @@ const ConditionGroup = ({taxonomies, value, onChange})=>{
     } = useTranslationWidgetTaxQuery()
 
     const {taxonomies: tax = [], relation = "OR"} = value;
-    
-    const handleChange = (_value)=> onChange(_value.taxonomies.length >= 1 ? _value : undefined);
+
+    const handleChange = (_value) => onChange(_value.taxonomies.length >= 1 ? _value : undefined);
 
     return <div>
         <div>
-            {tax.map((_tax, i)=>{
+            {tax.map((_tax, i) => {
                 return <ConditionWrapper key={i}>
                     <ConditionControl
                         taxonomies={taxonomies}
                         value={_tax}
-                        onChange={(changedTax)=>{
+                        onChange={(changedTax) => {
                             handleChange({
                                 ...value,
-                                taxonomies: tax.map((c, j)=> i === j ? changedTax : c)
+                                taxonomies: tax.map((c, j) => i === j ? changedTax : c)
                             })
                         }}
                     />
-                    <Button 
+                    <Button
                         isDestructive
                         isSmall
-                        onClick={()=>{
+                        onClick={() => {
                             handleChange({
                                 ...value,
                                 taxonomies: tax
-                                    .map((c,j)=> j === i ? null : c)
-                                    .filter(c=>c!=null)
+                                    .map((c, j) => j === i ? null : c)
+                                    .filter(c => c != null)
                             })
                         }}
                     >
@@ -175,15 +175,15 @@ const ConditionGroup = ({taxonomies, value, onChange})=>{
             })}
             {tax.length > 1 ?
                 <div
-                style={{
-                    marginTop: 10,
-                }}
+                    style={{
+                        marginTop: 10,
+                    }}
                 >
-                    <ToggleControl 
+                    <ToggleControl
                         label={relation}
-                        help={relation === "AND" ? toggle_AND_description: toggle_OR_description}
+                        help={relation === "AND" ? toggle_AND_description : toggle_OR_description}
                         checked={relation === "AND"}
-                        onChange={(checked)=>{
+                        onChange={(checked) => {
                             handleChange({
                                 ...value,
                                 relation: checked ? "AND" : "OR",
@@ -195,10 +195,10 @@ const ConditionGroup = ({taxonomies, value, onChange})=>{
                 null
             }
         </div>
-        <Button 
+        <Button
             isSecondary
             isSmall
-            onClick={()=>{
+            onClick={() => {
                 handleChange({
                     relation,
                     taxonomies: [
@@ -209,7 +209,7 @@ const ConditionGroup = ({taxonomies, value, onChange})=>{
             }}
             style={{
                 width: "100%",
-                textAlign:"center",
+                textAlign: "center",
                 display: "inline-block"
             }}
         >
@@ -219,11 +219,11 @@ const ConditionGroup = ({taxonomies, value, onChange})=>{
 }
 
 
-const TaxQueryWidget = ({definition, value, onChange})=> {
-    return <BaseControl 
+const TaxQueryWidget = ({definition, value, onChange}) => {
+    return <BaseControl
         label={definition.label}
     >
-        <ConditionGroup 
+        <ConditionGroup
             taxonomies={definition.taxonomies}
             value={value}
             onChange={onChange}
