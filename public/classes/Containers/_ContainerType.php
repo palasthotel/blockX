b7b3ebc8c60d70663c444bb9d7d144b0f3428533
EdwardBock <edward.bock@palasthotel.de>
2021-11-15 20:40:21 +0100

    container styles for frontend and backend

--- public/classes/Containers/_ContainerType.php
+++ public/classes/Containers/_ContainerType.php
@@ -7,41 +7,48 @@ use Palasthotel\WordPress\BlockX\Plugin;
 
 abstract class _ContainerType implements _IContainerType {
 
-	public function registerContainer(){
+	public function registerContainer() {
 		// build block.json
 		$bag = Plugin::instance()->bag;
-		$bag->createBlockJSONIfNotExists($this);
-		register_block_type( $bag->getBlockJSONFilePath($this) );
+		$bag->createBlockJSONIfNotExists( $this );
+		register_block_type( $bag->getBlockJSONFilePath( $this ) );
 
-		// build style.css
-		$styles = [
-			$this->style(),
-			$this->editorStyle(),
-		];
-		foreach ($styles as $style){
-			if($style->isGenerated()){
-				$bag->createContainerStylesIfNotExists($this, $style);
-				$path = $bag->getContainerStylesFilePath($this, $style);
-				$url = $bag->getContainerStylesUrl($this, $style);
-				wp_register_style(
-					(string) $style,
-					$url,
-					[Plugin::HANDLE_CSS_CONTAINER_BASE],
-					filemtime($path)
-				);
-			}
+		// build styles
+		$editorStyle = $this->editorStyle();
+		if ( $editorStyle->isGenerated() ) {
+			$bag->createContainerStylesIfNotExists( $this, $editorStyle, true );
+			$path = $bag->getContainerStylesFilePath( $this, $editorStyle );
+			$url  = $bag->getContainerStylesUrl( $this, $editorStyle );
+			wp_register_style(
+				(string) $editorStyle,
+				$url,
+				[ Plugin::HANDLE_CSS_CONTAINER_BASE ],
+				filemtime( $path )
+			);
+		}
+		$style = $this->style();
+		if ( $style->isGenerated() ) {
+			$bag->createContainerStylesIfNotExists( $this, $style, false );
+			$path = $bag->getContainerStylesFilePath( $this, $style );
+			$url  = $bag->getContainerStylesUrl( $this, $style );
+			wp_register_style(
+				(string) $style,
+				$url,
+				[ Plugin::HANDLE_CSS_CONTAINER_BASE ],
+				filemtime( $path )
+			);
 		}
 	}
 
 	public function style(): Style {
 		return Style::build(
-			$this->id()->namespace."_".$this->id()->name
+			$this->id()->namespace . "_" . $this->id()->name
 		);
 	}
 
 	public function editorStyle(): Style {
 		return Style::build(
-			$this->id()->namespace."_".$this->id()->name
+			$this->id()->namespace . "_" . $this->id()->name . "_editor"
 		);
 	}
 
@@ -51,6 +58,10 @@ abstract class _ContainerType implements _IContainerType {
 
 	public function useColumnsInMobilePreview(): bool {
 		return false;
+	}
+
+	public function breakpoint(): int{
+		return apply_filters(Plugin::FILTER_CONTAINER_BREAKPOINT, 900, $this);
 	}
 
 }
