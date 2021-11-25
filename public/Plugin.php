55ec3539383dfaea2779b3aeac889a89c54a81d0
EdwardBock <edward.bock@palasthotel.de>
2021-11-25 13:40:59 +0100

    compat

--- public/Plugin.php
+++ public/Plugin.php
@@ -8,7 +8,7 @@
  * Author: Palasthotel <rezeption@palasthotel.de> (in person: Edward Bock)
  * Author URI: http://www.palasthotel.de
  * Requires at least: 5.0
- * Tested up to: 5.8.1
+ * Tested up to: 5.8.2
  * Requires PHP: 7.3
  * Text Domain: blockx
  * License: http://www.gnu.org/licenses/gpl-2.0.html GPLv2
