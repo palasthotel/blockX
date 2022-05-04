e92b8ed20ed63abbea4203f995fe1c30fbd37faf
Sebastian Srb <sebastian.srb@palasthotel.de>
2022-05-04 16:08:05 +0200

    Cleanup

--- public/classes/ComposedBlocks/_ComposedBlockType.php
+++ public/classes/ComposedBlocks/_ComposedBlockType.php
@@ -8,21 +8,15 @@ use Palasthotel\WordPress\BlockX\Plugin;
 
 abstract class _ComposedBlockType implements _IComposedBlockType {
 
-	private array $templates = [];
-	private array $allowedBlocks = [];
-	private string $orientation = "vertical";
-	private string $templateLock = "";
-
-	public function __construct(int $breakpoint = 0) {
-		$this->breakpoint = $breakpoint;
-	}
+  private array $templates = [];
+  private array $allowedBlocks = [];
+  private string $orientation = "vertical";
+  private string $templateLock = "";
 
   public function registerComposedBlock() {
     // build block.json
     $bag = Plugin::instance()->bag;
     $bag->createComposedBlockJSONIfNotExists( $this );
-
-
 
     // so they can be enqueued
     $this->script();
@@ -37,35 +31,35 @@ abstract class _ComposedBlockType implements _IComposedBlockType {
     return "";
   }
 
-	public function styles(): Styles {
+  public function styles(): Styles {
     return Styles::build();
-	}
+  }
 
-	public function editorStyles(): Styles {
-		return Styles::build();
-	}
+  public function editorStyles(): Styles {
+    return Styles::build();
+  }
 
-	public function templates(): array{
-		return apply_filters(Plugin::FILTER_COMPOSED_BLOCKS_TEMPLATES, $this->templates, $this);
-	}
+  public function templates(): array{
+    return apply_filters(Plugin::FILTER_COMPOSED_BLOCKS_TEMPLATES, $this->templates, $this);
+  }
 
   public function allowedBlocks(): array{
-		return apply_filters(Plugin::FILTER_COMPOSED_BLOCKS_ALLOWED_BLOCKS, $this->allowedBlocks, $this);
-	}
+    return apply_filters(Plugin::FILTER_COMPOSED_BLOCKS_ALLOWED_BLOCKS, $this->allowedBlocks, $this);
+  }
 
   public function orientation(): string{
-		return apply_filters(Plugin::FILTER_COMPOSED_BLOCKS_ORIENTATION, $this->orientation, $this);
-	}
+    return apply_filters(Plugin::FILTER_COMPOSED_BLOCKS_ORIENTATION, $this->orientation, $this);
+  }
 
   public function templateLock(): string{
-		return apply_filters(Plugin::FILTER_COMPOSED_BLOCKS_TEMPLATE_LOCK, $this->templateLock, $this);
-	}
+    return apply_filters(Plugin::FILTER_COMPOSED_BLOCKS_TEMPLATE_LOCK, $this->templateLock, $this);
+  }
 
   public function icon(): string{
-		return "";
-	}
+    return "";
+  }
   public function category(): string{
-		return "";
-	}
+    return "";
+  }
 
 }
