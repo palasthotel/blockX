d582d3ce1be9e7be3408bef27e6d22ed44b95282
EdwardBock <edward.bock@palasthotel.de>
2022-05-08 13:26:02 +0200

    use block fix

--- src/lib/use-block.ts
+++ src/lib/use-block.ts
@@ -1,8 +1,8 @@
 import {get, set, setDeprecated} from "./store";
 
 export const registerUseBlock = (fn: any) => {
-    set("utils", "useBlocks", fn);
+    set("utils", "useBlock", fn);
     setDeprecated("useBlock", fn);
 }
 
-export const useBlock = get.bind(undefined, "utils", "useBlock");
+export const useBlock = ()=> get<Function>("utils", "useBlock")?.();
