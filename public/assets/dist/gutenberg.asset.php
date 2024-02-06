ae3a3fc227e2a33636309213c75a5e559766678e
Edward <edward.bock@palasthotel.de>
2024-02-06 16:05:56 +0100

    transpiled

--- public/assets/dist/gutenberg.asset.php
+++ public/assets/dist/gutenberg.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e64ceede0b136ce6f16b');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4dabcef92652e13d0f08');
