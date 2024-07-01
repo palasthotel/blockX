0ff049b3a9f83b2451878f36637dd1f7794446ba
Edward <edward.bock@palasthotel.de>
2024-07-01 19:16:47 +0200

    moved to tsup

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,6 @@
+import {ReactNode} from "react";
 
 export type BlockId = {namespace: string, name: string}
 export type WidgetType = string
 
-export type FunctionComponent = (props: any) => JSX.Element
+export type FunctionComponent = (props: any) => ReactNode
