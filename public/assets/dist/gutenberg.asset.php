94c29cae9f212e81bc4b1d71f31fd2443e1a8d2b
Edward <edward.bock@palasthotel.de>
2024-07-01 19:18:19 +0200

    ignore build files

--- public/assets/dist/gutenberg.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ab4911b85aa19b65b75a');
