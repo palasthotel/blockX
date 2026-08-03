6bdb6f6fd80af97d601c004ee2b0e79c50c3b77a
JanaME <jana.eggebrecht@palasthotel.de>
2026-08-03 15:52:44 +0200

    chore: make the DEV wrapper load on a case-sensitive filesystem
    It included public/plugin.php while the file is public/Plugin.php, so a checkout
    of wp-plugin/ into wp-content/plugins/ was a fatal error on Linux and worked only
    on case-insensitive filesystems like macOS. Domain Path pointed at
    /plugin/languages, which has never existed here. Also adds the ABSPATH guard and
    switches the author URL to https.
    
    The wrapper is never released, so nothing changes for users of the plugin.

--- wp-plugin/plugin.php
+++ wp-plugin/plugin.php
@@ -7,14 +7,16 @@
  * Version:           X.X.X
  * Requires at least: X.X
  * Tested up to:      X.X.X
- * Author:            PALASTHOTEL by Edward
- * Author URI:        http://www.palasthotel.de
- * Domain Path:       /plugin/languages
+ * Author:            Palasthotel <rezeption@palasthotel.de>
+ * Author URI:        https://palasthotel.de
+ * Domain Path:       /public/languages
  */
+
+defined( 'ABSPATH' ) || exit;
 
 use Palasthotel\WordPress\BlockX\Plugin;
 
-include dirname( __FILE__ ) . "/public/plugin.php";
+include dirname( __FILE__ ) . "/public/Plugin.php";
 
 register_activation_hook(__FILE__, function($multisite){
 	Plugin::instance()->onActivation($multisite);
